54273896093f4a449d266f454e2c0bc6a23a14c5
megvadulthangya <megvadulthangya@gmail.com>
2026-01-14 01:07:47 +0100

    change to real packages.py

--- packages.py
+++ packages.py
@@ -5,11 +5,50 @@ Package definitions for Manjaro Package Builder
 # LOCAL packages (from our repository)
 LOCAL_PACKAGES = [
     "gghelper",
-    "awesome-git"
+    "gtk2",
+    "awesome-freedesktop-git",
+    "lain-git",
+    "awesome-rofi",
+    "awesome-git",
+    "tilix-git",
+    "nordic-backgrounds",
+    "awesome-copycats-manjaro",
+    "i3lock-fancy-git",
+    "ttf-font-awesome-5",
+    "nvidia-driver-assistant",
+    "grayjay-bin"
 ]
 
 # AUR packages (from Arch User Repository)
 AUR_PACKAGES = [
+    "libinput-gestures",
+    "gtkd",
+    "qt5-styleplugins",
+    "urxvt-resize-font-git",
+    "i3lock-color",
+    "raw-thumbnailer",
+    "gsconnect",
+    "tamzen-font",
+    "betterlockscreen",
+    "nordic-theme",
+    "nordic-darker-theme",
+    "geany-nord-theme",
+    "nordzy-icon-theme",
+    "oh-my-posh-bin",
+    "fish-done",
+    "find-the-command",
+    "p7zip-gui",
+    "qownnotes",
+    "xorg-font-utils",
+    "xnviewmp",
+    "simplescreenrecorder",
+    "gtkhash-thunar",
+    "a4tech-bloody-driver-git",
+    "nordic-bluish-accent-theme",
+    "nordic-bluish-accent-standard-buttons-theme",
+    "nordic-polar-standard-buttons-theme",
+    "nordic-standard-buttons-theme",
+    "nordic-darker-standard-buttons-theme"
 ]
 
 # Optionally, you can also define package groups or categories
