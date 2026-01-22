4cb1f597e5bacf49fc09ded9df8583f2e2946527
megvadulthangya <megvadulthangya@gmail.com>
2026-01-22 20:45:30 +0100

    ...

--- .github/scripts/packages.py
+++ .github/scripts/packages.py
@@ -4,7 +4,7 @@ Package definitions for Manjaro Package Builder
 
 # LOCAL packages (from our repository)
 LOCAL_PACKAGES = [
-    "gghelper",
+    #"gghelper",
     "gtk2",
     "awesome-freedesktop-git",
     "lain-git",
