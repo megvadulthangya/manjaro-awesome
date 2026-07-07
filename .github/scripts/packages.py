4173a208f71ca5dd58cef10ad3298da42cec419c
megvadulthangya <megvadulthangya@gmail.com>
2026-07-07 19:30:23 +0200

    add manjaro-awesome-keyring package

--- .github/scripts/packages.py
+++ .github/scripts/packages.py
@@ -19,6 +19,7 @@ LOCAL_PACKAGES = [
     "stable-diffusion-webui-forge-cu124",
     "stable-diffusion-webui-forge-neo-git",
     "picom-pijulius-next-git",
+    "manjaro-awesome-keyring",
     "moneymanagerex",
     "nordic-theme-git",
     "nordic-theme",
