27d228d8fae7bd28468e58f9345fc0b092bf51f2
megvadulthangya <megvadulthangya@gmail.com>
2026-07-31 22:34:11 +0200

    add gvc-git

--- .github/scripts/packages.py
+++ .github/scripts/packages.py
@@ -6,6 +6,7 @@ Package definitions for Manjaro Package Builder
 LOCAL_PACKAGES = [
 #    "capitaine-cursors-sainnhe-git",
     "manjaro-live",
+    "gvc-git",
     "gsconnect",
     "cuda-12.9",
     "cudnn9.10-cuda12.9",
