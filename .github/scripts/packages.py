1280351d89420254386e78a817465535ac9f9f80
megvadulthangya <megvadulthangya@gmail.com>
2026-08-07 14:06:47 +0200

    add lua-lgi-git package

--- .github/scripts/packages.py
+++ .github/scripts/packages.py
@@ -69,6 +69,7 @@ LOCAL_PACKAGES = [
 # AUR packages (from Arch User Repository)
 AUR_PACKAGES = [
     "libinput-gestures",
+    "lua-lgi-git",
     "gvc-git",
 #    "picom-pijulius-next-git",
     "gtkd",
