1f10616af76ec1f946cb790bb5ddff50ff2022e6
megvadulthangya <megvadulthangya@gmail.com>
2026-08-10 11:50:27 +0200

    add manjaro-xfce-settings to build

--- .github/scripts/packages.py
+++ .github/scripts/packages.py
@@ -5,6 +5,7 @@ Package definitions for Manjaro Package Builder
 # LOCAL packages (from our repository)
 LOCAL_PACKAGES = [
 #    "capitaine-cursors-sainnhe-git",
+    "manjaro-xfce-settings",
     "manjaro-live",
     "gsconnect",
     "cuda-12.9",
