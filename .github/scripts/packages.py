52c3bd485fce4c6b81b46ad2ba775f915f4c8d87
megvadulthangya <megvadulthangya@gmail.com>
2026-09-19 13:18:15 +0200

    remove rofi 1.7 from built packages

--- .github/scripts/packages.py
+++ .github/scripts/packages.py
@@ -5,7 +5,7 @@ Package definitions for Manjaro Package Builder
 # LOCAL packages (from our repository)
 LOCAL_PACKAGES = [
 #    "capitaine-cursors-sainnhe-git",
-    "rofi",
+#    "rofi",
     "rofi-git",
     "awesome-updater",
     "xlibre-xserver-legacyabi",
